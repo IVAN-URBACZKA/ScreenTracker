27dc12f60a577f4a4fa9ae98d0b09331eb00831f
IVAN-URBACZKA <ivan.urbaczka@gmail.com>
2023-12-25 17:21:31 +0100

    add css moviedetails

--- screen_tracker/src/components/MovieDetails.jsx
+++ screen_tracker/src/components/MovieDetails.jsx
@@ -10,6 +10,7 @@ const MovieDetails = () => {
         const fetchMovieDetails = async () => {
             const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=55b2ede0aaf742708c6b67e287f72d75`);
             const data = await response.json();
+            console.log(data);
             setMovie(data);
         };
 
@@ -17,14 +18,28 @@ const MovieDetails = () => {
     }, [id]);
 
     return (
-        <div className="movie-details">
-            <h1>{movie.title}</h1>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <p><strong>Description:</strong> {movie.overview}</p>
-            <p><strong>Date de sortie:</strong> {movie.release_date}</p>
-            <p><strong>Note moyenne:</strong> {movie.vote_average} / 10</p>
-            <p><strong>Nombre de votes:</strong> {movie.vote_count}</p>
-            {/* Vous pouvez ajouter plus de détails ici si disponibles */}
+        <div className="movie-container">
+            <div className="movie-header" style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
+                <h1>{movie.title}</h1>
+            </div>
+            <div className="movie-details">
+                <div className="movie-poster">
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                </div>
+                <div className="movie-info">
+                    <p className="movie-overview">{movie.overview}</p>
+                    <div className="movie-meta">
+                        <span>Budget: ${movie.budget}</span>
+                        <span>Revenue: ${movie.revenue}</span>
+                        <span>Runtime: {movie.runtime} minutes</span>
+                    </div>
+                    <div className="movie-genres">
+                        {movie.genres && movie.genres.map(genre => <span key={genre.id}>{genre.name}</span>)}
+                    </div>
+                    <a href={movie.homepage} target="_blank" rel="noopener noreferrer" className="movie-link">Official Website</a>
+                    <button className="trailer-button">Watch Trailer</button>
+                </div>
+            </div>
         </div>
     );
 };
